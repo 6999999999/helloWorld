0e27cfc7cdd265acaf73f43931ed0280a297a57e
ravindra <ravindra.hajare@deccanlogic.com>
2023-12-04 12:34:12 +0530

    hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+// Importing the HTTP module
+const http = require('http');
+const port = 3000;
+
+// Creating the server
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
